23d0cdd910119dfb5714a580ce07f2b02b7d51dd
Nicole Kleinhoff <ilbelkyr@shalture.org>
2021-02-18 21:21:02 +0000

    projectns: use contacts' visible/secondary flag
    This will list the different types of contact in projectns/info output
    as well as in nickserv/info output for staff and the user themselves.
    
    The chanserv/info output will include a list of public contacts, if any;
    for staff, private contacts will also be listed and the list will note
    the "secondary" flag on any listed contacts.
    
    The projectns/contact interface has also been extended to allow adding
    contacts with the public or secondary flag. These flags can also be
    modified on existing contacts.

--- projectns/contact.c
+++ projectns/contact.c
@@ -12,19 +12,49 @@
 
 static void cmd_contact(sourceinfo_t *si, int parc, char *parv[]);
 
-command_t ps_contact = { "CONTACT", N_("Manages project contacts."), PRIV_PROJECT_ADMIN, 3, cmd_contact, { .path = "freenode/project_contact" } };
+command_t ps_contact = { "CONTACT", N_("Manages project contacts."), PRIV_PROJECT_ADMIN, 5, cmd_contact, { .path = "freenode/project_contact" } };
 
 static void cmd_contact(sourceinfo_t *si, int parc, char *parv[])
 {
 	char *project   = parv[0];
 	char *mode      = parv[1];
 	char *target    = parv[2];
+	char **extra    = parv + 3;
 
 	enum {
 		CONTACT_BAD = 0,
 		CONTACT_ADD,
 		CONTACT_DEL,
+		CONTACT_SET,
 	} add_or_del = CONTACT_BAD;
+
+	enum {
+		VIS_UNSPEC = 0,
+		VIS_PUBLIC,
+		VIS_PRIVATE,
+	} change_visible = VIS_UNSPEC;
+
+	enum {
+		GC_UNSPEC = 0,
+		GC_PRIMARY,
+		GC_SECONDARY,
+	} change_secondary = GC_UNSPEC;
+
+	bool bad_params = false;
+
+	for (unsigned char i = 0; extra[i] != NULL; i++)
+	{
+		if (strcasecmp(extra[i], "PUBLIC") == 0 && !change_visible)
+			change_visible = VIS_PUBLIC;
+		else if (strcasecmp(extra[i], "PRIVATE") == 0 && !change_visible)
+			change_visible = VIS_PRIVATE;
+		else if (strcasecmp(extra[i], "PRIMARY") == 0 && !change_secondary)
+			change_secondary = GC_PRIMARY;
+		else if (strcasecmp(extra[i], "SECONDARY") == 0 && !change_secondary)
+			change_secondary = GC_SECONDARY;
+		else
+			bad_params = true;
+	}
 
 	if (mode)
 	{
@@ -32,17 +62,26 @@ static void cmd_contact(sourceinfo_t *si, int parc, char *parv[])
 			add_or_del = CONTACT_ADD;
 		else if (strcasecmp(mode, "DEL") == 0)
 			add_or_del = CONTACT_DEL;
+		else if (strcasecmp(mode, "SET") == 0)
+			add_or_del = CONTACT_SET;
+		else
+			bad_params = true;
 	}
 
-	if (!target || !add_or_del)
+	bool settings_given = change_visible || change_secondary;
+
+	if ((add_or_del == CONTACT_DEL && settings_given) || (add_or_del == CONTACT_SET && !settings_given))
+		bad_params = true;
+
+	if (bad_params || !target)
 	{
-		cmd_faultcode_t fault = (target ? fault_badparams : fault_needmoreparams);
+		cmd_faultcode_t fault = (bad_params ? fault_badparams : fault_needmoreparams);
 
 		if (fault == fault_badparams)
 			command_fail(si, fault, STR_INVALID_PARAMS, "CONTACT");
 		else
 			command_fail(si, fault, STR_INSUFFICIENT_PARAMS, "CONTACT");
-		command_fail(si, fault, _("Syntax: CONTACT <project> ADD|DEL <account>"));
+		command_fail(si, fault, _("Syntax: CONTACT <project> ADD|DEL|SET <account> [PRIVATE|PUBLIC] [PRIMARY|SECONDARY]"));
 		return;
 	}
 
@@ -64,14 +103,85 @@ static void cmd_contact(sourceinfo_t *si, int parc, char *parv[])
 
 	if (add_or_del == CONTACT_ADD)
 	{
-		if (projectsvs->contact_new(p, mu))
+		struct project_contact *c = projectsvs->contact_new(p, mu);
+		if (c != NULL)
 		{
-			logcommand(si, CMDLOG_ADMIN, "PROJECT:CONTACT:ADD: \2%s\2 to \2%s\2", entity(mu)->name, p->name);
-			command_success_nodata(si, _("\2%s\2 was set as a contact for project \2%s\2."), entity(mu)->name, p->name);
+			if (change_visible == VIS_PUBLIC)
+				c->visible = true;
+			if (change_secondary == GC_SECONDARY)
+				c->secondary = true;
+
+			logcommand(si, CMDLOG_ADMIN, "PROJECT:CONTACT:ADD: \2%s\2 to \2%s\2 (%s, %s)", entity(mu)->name, p->name, c->secondary ? "secondary" : "primary", c->visible ? "public" : "private");
+
+			if (c->secondary)
+			{
+				if (c->visible)
+					command_success_nodata(si, _("\2%s\2 was set as a secondary contact for project \2%s\2 (user-visible)."), entity(mu)->name, p->name);
+				else
+					command_success_nodata(si, _("\2%s\2 was set as a secondary contact for project \2%s\2 (only visible to staff)."), entity(mu)->name, p->name);
+			}
+			else
+			{
+				if (c->visible)
+					command_success_nodata(si, _("\2%s\2 was set as a primary contact for project \2%s\2 (user-visible)."), entity(mu)->name, p->name);
+				else
+					command_success_nodata(si, _("\2%s\2 was set as a primary contact for project \2%s\2 (only visible to staff)."), entity(mu)->name, p->name);
+			}
 		}
 		else
 		{
 			command_fail(si, fault_nochange, _("\2%s\2 is already listed as contact for project \2%s\2."), entity(mu)->name, p->name);
+		}
+	}
+	else if (add_or_del == CONTACT_SET)
+	{
+		mowgli_node_t *n;
+		struct project_contact *c;
+		MOWGLI_ITER_FOREACH(n, p->contacts.head)
+		{
+			c = n->data;
+			if (c->mu == mu)
+				break;
+		}
+
+		if (!n)
+		{
+			command_fail(si, fault_nosuch_key, _("\2%s\2 was not listed as a contact for project \2%s\2."), entity(mu)->name, p->name);
+		}
+		else
+		{
+			if ((c->visible && change_visible == VIS_PUBLIC) || (!c->visible && change_visible == VIS_PRIVATE))
+				change_visible = VIS_UNSPEC;
+			if ((c->secondary && change_secondary == GC_SECONDARY) || (!c->secondary && change_secondary == GC_PRIMARY))
+				change_secondary = GC_UNSPEC;
+
+			if (change_visible == VIS_PUBLIC)
+			{
+				c->visible = true;
+				logcommand(si, CMDLOG_ADMIN, "PROJECT:CONTACT:SET: \2%s\2 on \2%s\2 to %s", entity(mu)->name, p->name, "public");
+				command_success_nodata(si, _("\2%s\2 is now listed as a public contact for project \2%s\2."), entity(mu)->name, p->name);
+			}
+			else if (change_visible == VIS_PRIVATE)
+			{
+				c->visible = false;
+				logcommand(si, CMDLOG_ADMIN, "PROJECT:CONTACT:SET: \2%s\2 on \2%s\2 to %s", entity(mu)->name, p->name, "private");
+				command_success_nodata(si, _("\2%s\2 is now only displayed to staff as contact for project \2%s\2."), entity(mu)->name, p->name);
+			}
+			if (change_secondary == GC_SECONDARY)
+			{
+				c->secondary = true;
+				logcommand(si, CMDLOG_ADMIN, "PROJECT:CONTACT:SET: \2%s\2 on \2%s\2 to %s", entity(mu)->name, p->name, "secondary");
+				command_success_nodata(si, _("\2%s\2 is now considered a secondary contact for project \2%s\2."), entity(mu)->name, p->name);
+			}
+			else if (change_secondary == GC_PRIMARY)
+			{
+				c->secondary = false;
+				logcommand(si, CMDLOG_ADMIN, "PROJECT:CONTACT:SET: \2%s\2 on \2%s\2 to %s", entity(mu)->name, p->name, "primary");
+				command_success_nodata(si, _("\2%s\2 is now considered a primary contact for project \2%s\2."), entity(mu)->name, p->name);
+			}
+
+			if (!change_visible && !change_secondary)
+				command_fail(si, fault_nochange, _("Settings for \2%s\2 as a contact for project \2%s\2 were not changed."), entity(mu)->name, p->name);
 		}
 	}
 	else // CONTACT_DEL
--- projectns/info.c
+++ projectns/info.c
@@ -14,6 +14,37 @@ static void cmd_info(sourceinfo_t *si, int parc, char *parv[]);
 
 command_t ps_info = { "INFO", N_("Displays information about a project registration."), PRIV_PROJECT_AUSPEX, 1, cmd_info, { .path = "freenode/project_info" } };
 
+struct info_item
+{
+	sourceinfo_t *si;
+	const char *title;
+	unsigned int count;
+	char buf[BUFSIZE];
+};
+
+static void info_item_add(struct info_item *item, const char *text)
+{
+	item->count++;
+	if (strlen(item->buf) > 80)
+	{
+		command_success_nodata(item->si, _("%s: %s"), item->title, item->buf);
+		item->buf[0] = '\0';
+	}
+	if (item->buf[0])
+		mowgli_strlcat(item->buf, ", ", sizeof item->buf);
+	mowgli_strlcat(item->buf, text, sizeof item->buf);
+}
+
+static void info_item_done(struct info_item *item, bool note_empty)
+{
+	if (item->buf[0])
+		command_success_nodata(item->si, _("%s: %s"), item->title, item->buf);
+	else if (note_empty)
+		command_success_nodata(item->si, _("%s: %s"), item->title, "(none)");
+
+	item->buf[0] = '\0';
+}
+
 static void cmd_info(sourceinfo_t *si, int parc, char *parv[])
 {
 	char *name = parv[0];
@@ -50,55 +81,88 @@ static void cmd_info(sourceinfo_t *si, int parc, char *parv[])
 	else if (p->creator)
 		command_success_nodata(si, _("Registered by %s"), p->creator);
 
-	buf[0] = '\0';
+	struct info_item info =
+	{
+		.si    = si,
+		.title = "Channel namespaces",
+		.count = 0,
+		.buf   = { 0 },
+	};
 
 	mowgli_node_t *n;
 	MOWGLI_ITER_FOREACH(n, p->channel_ns.head)
 	{
-		if (strlen(buf) > 80)
-		{
-			command_success_nodata(si, _("Channel namespaces: %s"), buf);
-			buf[0] = '\0';
-		}
-		if (buf[0])
-			mowgli_strlcat(buf, ", ", sizeof buf);
-		mowgli_strlcat(buf, (const char*)n->data, sizeof buf);
+		info_item_add(&info, n->data);
 	}
+	info_item_done(&info, true);
 
-	command_success_nodata(si, _("Channel namespaces: %s"), (buf[0] ? buf : "(none)"));
-
-	buf[0] = '\0';
+	info.title = "Cloak namespaces";
+	info.count = 0;
 
 	MOWGLI_ITER_FOREACH(n, p->cloak_ns.head)
 	{
-		if (strlen(buf) > 80)
-		{
-			command_success_nodata(si, _("Cloak namespaces: %s"), buf);
-			buf[0] = '\0';
-		}
-		if (buf[0])
-			mowgli_strlcat(buf, ", ", sizeof buf);
-		mowgli_strlcat(buf, (const char*)n->data, sizeof buf);
+		info_item_add(&info, n->data);
 	}
+	info_item_done(&info, true);
 
-	command_success_nodata(si, _("Cloak namespaces: %s"), (buf[0] ? buf : "(none)"));
-
-	buf[0] = '\0';
+	info.title = "Group contacts (public)";
+	info.count = 0;
 
 	MOWGLI_ITER_FOREACH(n, p->contacts.head)
 	{
 		struct project_contact *contact = n->data;
-		if (strlen(buf) > 80)
-		{
-			command_success_nodata(si, _("Group contacts: %s"), buf);
-			buf[0] = '\0';
-		}
-		if (buf[0])
-			mowgli_strlcat(buf, ", ", sizeof buf);
-		mowgli_strlcat(buf, ((myentity_t*)contact->mu)->name, sizeof buf);
+		if (contact->secondary)
+			continue;
+		if (!contact->visible)
+			continue;
+		info_item_add(&info, entity(contact->mu)->name);
 	}
+	info_item_done(&info, false);
 
-	command_success_nodata(si, _("Group contacts: %s"), (buf[0] ? buf : "(none)"));
+	info.title = "Group contacts (private)";
+	// let count accumulate
+
+	MOWGLI_ITER_FOREACH(n, p->contacts.head)
+	{
+		struct project_contact *contact = n->data;
+		if (contact->secondary)
+			continue;
+		if (contact->visible)
+			continue;
+		info_item_add(&info, entity(contact->mu)->name);
+	}
+	info_item_done(&info, false);
+
+	if (!info.count)
+		command_success_nodata(si, _("Group contacts: (none)"));
+
+	info.title = "Secondary contacts (public)";
+	info.count = 0;
+
+	MOWGLI_ITER_FOREACH(n, p->contacts.head)
+	{
+		struct project_contact *contact = n->data;
+		if (!contact->secondary)
+			continue;
+		if (!contact->visible)
+			continue;
+		info_item_add(&info, entity(contact->mu)->name);
+	}
+	info_item_done(&info, false);
+
+	info.title = "Secondary contacts (private)";
+	info.count = 0;
+
+	MOWGLI_ITER_FOREACH(n, p->contacts.head)
+	{
+		struct project_contact *contact = n->data;
+		if (!contact->secondary)
+			continue;
+		if (contact->visible)
+			continue;
+		info_item_add(&info, entity(contact->mu)->name);
+	}
+	info_item_done(&info, false);
 
 	if (p->reginfo)
 		command_success_nodata(si, _("\"See also\" displayed when registering channels: %s"), p->reginfo);
